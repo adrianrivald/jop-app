876b59672d73fae2f979fcd0aa1271c5bdce513e
Adrian <adrian@Adrians-MacBook-Pro.local>
2022-08-03 08:52:24 +0700

    enc: landing page revision

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,20 +1,14 @@
 import logo from '../../logo.svg';
 import '../../App.scss';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Edit <code>src/App.js</code> and save to reload.</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <p>Welcome to JOP Landing Page</p>
+                <Link style={{marginTop: '12px'}} className="App-link" to="/auth">Go to signin page</Link>
             </header>
         </div>
     )
