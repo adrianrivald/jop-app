12794dbc36d8878016e699fabf5b6a58c1275f34
Adrian <adrian@Adrians-MacBook-Pro.local>
2022-08-08 20:29:13 +0700

    fix dropdown styling

--- src/components/forms/Dropdown.js
+++ src/components/forms/Dropdown.js
@@ -4,7 +4,7 @@ export default function DropDown({
 }) {
   return (
     <div>
-        <select onChange={onChange} className="bg-white-50 border border-gray-300 text-black-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <select onChange={onChange} className="bg-white-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-flora-500 focus:border-flora-500 block w-full p-2.5">
           {
             option.map((res, idx) => {
               return (
