a63ec27970fdf5af241a2c898f22a15b83fc99dc
Adrian Rivaldy <adrian.rivaldy@dataon.com>
2022-09-26 22:38:56 +0700

    logistic page

--- src/App.js
+++ src/App.js
@@ -9,12 +9,9 @@ import { Provider} from "react-redux";
 import store from "./store"
 import HomePage from './pages/homepage';
 import SignIn from './pages/auth/login';
-import Sample from './pages/sample/Sample';
 import Storybook from './pages/storybook';
-import Mandor from './pages/assignment/mandor';
 import MandorAcceptAssignment from './pages/assignment/mandor/accept';
 import MandorDiversionAssignment from './pages/assignment/mandor/diversion';
-import Mabes from './pages/assignment/mabes';
 import MabesAssignment from './pages/assignment/mabes/new-assignment';
 import MabesDetail from './pages/assignment/mabes/detail';
 import MabesDetailAction from './pages/assignment/mabes/detail/action';
@@ -26,14 +23,13 @@ import AbsenceList from './pages/absence/list';
 import AbsenceIn from './pages/absence/scan/in';
 import AbsenceOut from './pages/absence/scan/out';
 import TapperPlanning from './pages/assignment/mandor/tapper';
-import Balanced from './pages/balanced/index';
 import AddBalanced from './pages/balanced/add';
-import MandorWeighing from './pages/weighing/mandor';
 import DetailWeighing from './pages/weighing/mandor/detail';
 import WeighingTapper from './pages/weighing/mandor/tapper';
 import WeighingScan from './pages/weighing/mandor/detail/scan';
 import Assignment from './pages/assignment';
 import Weighing from './pages/weighing';
+import Logistic from './pages/logistic';
 
 function App() {
   const cookies = new Cookies();
@@ -57,11 +53,9 @@ function App() {
           <Route path="/" exact element={<Navigate to="/auth/login" replace />} />  {/* later add condition for auth */}
           <Route path="/auth/login" exact element={<SignIn/>} />
           <Route path="/homepage" exact element={<HomePage/>} />
-          <Route path="/sample" exact element={<Sample/>} />
           
           {/* penugasan */}
           <Route path="/assignment" element={<Assignment />} />
-          {/* <Route path="/assignment" element={role === "mandor" ? <Mandor /> : <Mabes/>} /> */}
           <Route path="/assignment/detail/:id/accept" exact element={<MandorAcceptAssignment />} />
           <Route path="/assignment/detail/:id/diversion" exact element={<MandorDiversionAssignment />} />
           <Route path="/assignment/tapper/:id" exact element={<TapperPlanning />} />
@@ -80,11 +74,13 @@ function App() {
           
           {/* penimbangan */}
           <Route path="/weighing" exact element={<Weighing />} />
-          {/* <Route path="/weighing" exact element={role === "mandor" ?  <MandorWeighing /> : <Balanced />} /> */}
           <Route path="/weighing/add" exact element={<AddBalanced />} />
           <Route path="/weighing/detail/:id" exact element={<DetailWeighing />} />
           <Route path="/weighing/detail/:id/tapper/:id_tapper" exact element={<WeighingTapper />} />
           <Route path="/weighing/detail/:id/scan" exact element={<WeighingScan />} />
+
+          {/* pengiriman */}
+          <Route path="/logistic" exact element={<Logistic />} />
               
           <Route path="/storybook" exact element={<Storybook/>} />
           <Route path="*" exact element={<div className='flex justify-center p-10'>Route not found</div>} />
---
+++ src/pages/logistic/index.js
@@ -0,0 +1,376 @@
+import axios from "axios";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
+import DatePicker from "../../components/forms/DatePicker";
+import DropDown from "../../components/forms/Dropdown";
+import Divider from "../../components/ui/Divider";
+import Header from "../../components/ui/Header"
+
+
+
+const url = process.env.REACT_APP_API_URL;
+
+function Dropdown (props) {
+    return (
+        <div className={`${props.customClass} w-full`}>
+            <h2 className='text-left mb-1'>{props.title}</h2>
+            <DropDown defaultValue={props.defaultValue} onChange={props.onChange} option={props.option} />
+        </div>
+    )
+}
+
+function FilledCircle () {
+    return (
+        <div className="w-1/4">
+            <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12.2227" cy="12" r="12" fill="#E96E1D"/>
+            </svg>
+        </div>
+    )
+}
+
+function OutlinedCircle () {
+    return (
+        <div className="w-1/4">
+            <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12.2227" cy="12" r="11" stroke="#A7A29A" stroke-width="2"/>
+            </svg>
+        </div>
+    )
+}
+function Logistic () {
+    const navigate = useNavigate();
+    const cookies = new Cookies();
+    const token = cookies.get('token');
+    const [tphList, setTphList] = React.useState([]);
+    const [selectedTph, setSelectedTph] = React.useState('');
+    const [dateFrom, setDateFrom] = React.useState('')
+    const [dateTo, setDateTo] = React.useState('')
+    const [batchReadyToDeliver, setBatchReadyToDeliver] = React.useState([]);
+    const [batchOnDelivery, setBatchOnDelivery] = React.useState([]);
+    const [batchDelivered, setBatchDelivered] = React.useState([]);
+
+    React.useEffect(() => {
+        getTPH();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
+    const getTPH = () => {
+        axios.get(`${url}tph/list?include=wilayah_tugas,divisi&sort=kode,nama`, {
+            url: process.env.REACT_APP_API_URL,
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json'
+            }
+        }).then((res) => {
+            const data = res.data.data.data
+            const tphData = data.map((res) => {
+                return {
+                    value: res.id,
+                    label: res.nama
+                }
+            })
+            setTphList(tphData)
+        })
+    }
+
+    const onChangeTph = (e) => {
+        setSelectedTph(e.target.value)
+    }
+
+    const onChangePeriod = (e, type) => {
+        if (type === 'from') {
+            setDateFrom(e.target.value)
+        } else {
+            setDateTo(e.target.value)
+        }
+    }
+
+    React.useEffect(() => {
+        if (dateFrom && dateTo) {
+            getBatchReadyToDeliver();
+            getBatchOnDelivery();
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [dateFrom,dateTo,selectedTph])
+
+    
+    const getBatchReadyToDeliver = () => {
+        axios.get(`${url}pengiriman/batch-siap-kirim?filter[tph]=${selectedTph}&filter[periode_penimbangan]=${dateFrom.concat(',',dateTo)}`, {
+            url: process.env.REACT_APP_API_URL,
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json'
+            }
+        }).then((res) => {
+            const data = res.data.data.data
+            setBatchReadyToDeliver(data)
+        })
+    }
+
+    const getBatchOnDelivery = () => {
+        axios.get(`${url}pengiriman/batch-dalam-pengiriman?filter[tph]=${selectedTph}&filter[periode_penimbangan]=${dateFrom.concat(',',dateTo)}`, {
+            url: process.env.REACT_APP_API_URL,
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json'
+            }
+        }).then((res) => {
+            const data = res.data.data.data
+            setBatchOnDelivery(data)
+        })
+    }
+
+    const getBatchDelivered = () => {
+        axios.get(`${url}pengiriman/batch-selesai-kirim?filter[tph]=${selectedTph}&filter[periode_penimbangan]=${dateFrom.concat(',',dateTo)}`, {
+            url: process.env.REACT_APP_API_URL,
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json'
+            }
+        }).then((res) => {
+            const data = res.data.data.data
+            setBatchDelivered(data)
+        })
+    }
+
+    return (
+        <>
+            <div className="header">
+                <Header title="Logistik" isWithBack/>
+            </div>
+            <div className="container">
+                <Dropdown title="Masukan kode lokasi awal" defaultValue="Pilih kode lokasi awal" option={tphList} onChange={(e) => onChangeTph(e)} />
+                <div>
+                    <h2 className='text-left mt-5 mb-1'>Atur Periode</h2>
+                    <div className='flex justify-between gap-2'>
+                            <div className='flex-auto w-64'>
+                                <DatePicker onChange={(e) => onChangePeriod(e, 'from')} />
+                            </div>
+                            <div className='flex-auto w-64'>
+                                <DatePicker onChange={(e) => onChangePeriod(e, 'to')} />
+                            </div>
+                    </div>
+                </div>
+                <Divider />
+                <div className="batch mt-5">
+                    <div className="flex justify-between items-center">
+                        <div className="w-2/4">
+                            <p className="font-bold text-xs">Batch siap kirim</p>
+                        </div>
+                        {
+                            batchReadyToDeliver?.length > 0 && (
+                                <div className="w-2/4 ml-2">
+                                    <div className="flex gap-6">
+                                        <p className="w-1/4">P1</p>
+                                        <p className="w-1/4">P2</p>
+                                        <p className="w-1/4">P3</p>
+                                        <p className="w-1/4">P4</p>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div> 
+                    {
+                        batchReadyToDeliver?.length > 0 ? batchReadyToDeliver?.map((res, idx) => {
+                            return (
+                                <div className="flex justify-between items-center mt-3">
+                                    <div className="w-2/4">
+                                        <div className="flex items-center">
+                                            <span>{res?.kode.split("-")[0]}</span>
+                                            <input className="ml-2 rounded-lg p-2 text-xs  focus:outline-none focus:shadow-outline" type="text" readOnly value={res?.batch}/>
+                                        </div>
+                                    </div>
+                                    <div className="w-2/4">
+                                        <div className="flex gap-6">
+                                            {
+                                                res?.detail?.map((res, idx) => {
+                                                    if ( 
+                                                        res?.kode === 'P1' ||
+                                                        res?.kode === 'P2' ||
+                                                        res?.kode === 'P3' ||
+                                                        res?.kode === 'P4'
+                                                    ) {
+                                                        return (
+                                                            <FilledCircle />
+                                                        )
+                                                    } else if (
+                                                    
+                                                        res?.kode !== 'P1' ||
+                                                        res?.kode !== 'P2' ||
+                                                        res?.kode !== 'P3' ||
+                                                        res?.kode !== 'P4'
+                                                    ) { 
+                                                        return (
+                                                            <OutlinedCircle />
+                                                        )
+                                                    }
+                                                    return <OutlinedCircle />
+                                                }
+                                            )
+                                        }
+                                        {
+                                            res?.detail?.length < 4 && (
+                                                <OutlinedCircle />
+                                            )
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }) : (
+                            <div className="flex justify-center mt-3">No Data</div>
+                        )
+                    }
+                </div>
+                <Divider />
+                <div className="batch mt-5">
+                    <div className="flex justify-between items-center">
+                        <div className="w-2/4">
+                            <p className="font-bold text-xs">Batch sedang dalam pengiriman</p>
+                        </div>
+                        {
+                            batchOnDelivery?.length > 0 && (
+                                <div className="w-2/4 ml-2">
+                                    <div className="flex gap-6">
+                                        <p className="w-1/4">P1</p>
+                                        <p className="w-1/4">P2</p>
+                                        <p className="w-1/4">P3</p>
+                                        <p className="w-1/4">P4</p>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                    {
+                        batchOnDelivery?.length > 0 ? batchOnDelivery?.map((res, idx) => {
+                            return (
+                                <div className="flex justify-between items-center mt-3">
+                                    <div className="w-2/4">
+                                        <div className="flex items-center">
+                                            <span>{res?.kode.split("-")[0]}</span>
+                                            <input className="ml-2 rounded-lg p-2 text-xs  focus:outline-none focus:shadow-outline" type="text" readOnly value={res?.batch}/>
+                                        </div>
+                                    </div>
+                                    <div className="w-2/4">
+                                        <div className="flex gap-6">
+                                            {
+                                                res?.detail?.map((res, idx) => {
+                                                    if ( 
+                                                        res?.kode === 'P1' ||
+                                                        res?.kode === 'P2' ||
+                                                        res?.kode === 'P3' ||
+                                                        res?.kode === 'P4'
+                                                    ) {
+                                                        return (
+                                                            <FilledCircle />
+                                                        )
+                                                    } else if (
+                                                    
+                                                        res?.kode !== 'P1' ||
+                                                        res?.kode !== 'P2' ||
+                                                        res?.kode !== 'P3' ||
+                                                        res?.kode !== 'P4'
+                                                    ) { 
+                                                        return (
+                                                            <OutlinedCircle />
+                                                        )
+                                                    }
+                                                    return <OutlinedCircle />
+                                                }
+                                            )
+                                        }
+                                        {
+                                            res?.detail?.length < 4 && (
+                                                <OutlinedCircle />
+                                            )
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }) : (
+                            <div className="flex justify-center mt-3">No Data</div>
+                        )
+                    }
+                <Divider />
+                </div>
+                <div className="batch mt-5">
+                    <div className="flex justify-between items-center">
+                        <div className="w-2/4">
+                            <p className="font-bold text-xs">Batch selesai terkirim</p>
+                        </div>
+                        {
+                            batchDelivered?.length > 0 && (
+                                <div className="w-2/4 ml-2">
+                                    <div className="flex gap-6">
+                                        <p className="w-1/4">P1</p>
+                                        <p className="w-1/4">P2</p>
+                                        <p className="w-1/4">P3</p>
+                                        <p className="w-1/4">P4</p>
+                                    </div>
+                                </div> 
+                            )
+                        }
+                    </div>
+                    {
+                        batchDelivered?.length > 0 ? batchDelivered?.map((res, idx) => {
+                            return (
+                                <div className="flex justify-between items-center mt-3">
+                                    <div className="w-2/4">
+                                        <div className="flex items-center">
+                                            <span>{res?.kode.split("-")[0]}</span>
+                                            <input className="ml-2 rounded-lg p-2 text-xs  focus:outline-none focus:shadow-outline" type="text" readOnly value={res?.batch}/>
+                                        </div>
+                                    </div>
+                                    <div className="w-2/4">
+                                        <div className="flex gap-6">
+                                            {
+                                                res?.detail?.map((res, idx) => {
+                                                    if ( 
+                                                        res?.kode === 'P1' ||
+                                                        res?.kode === 'P2' ||
+                                                        res?.kode === 'P3' ||
+                                                        res?.kode === 'P4'
+                                                    ) {
+                                                        return (
+                                                            <FilledCircle />
+                                                        )
+                                                    } else if (
+                                                    
+                                                        res?.kode !== 'P1' ||
+                                                        res?.kode !== 'P2' ||
+                                                        res?.kode !== 'P3' ||
+                                                        res?.kode !== 'P4'
+                                                    ) { 
+                                                        return (
+                                                            <OutlinedCircle />
+                                                        )
+                                                    }
+                                                    return <OutlinedCircle />
+                                                }
+                                            )
+                                        }
+                                        {
+                                            res?.detail?.length < 4 && (
+                                                <OutlinedCircle />
+                                            )
+                                        }
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }) : (
+                            <div className="flex justify-center mt-3">No Data</div>
+                        )
+                    }
+                </div>
+
+            </div>
+        </>
+        
+    )
+}
+
+export default Logistic
