9c4b4a04404e2330a6b27439a42bc3941f530474
Adrian Rivaldy <adrian.rivaldy@dataon.com>
2022-11-01 22:58:35 +0700

    fix modal width

--- src/components/ui/Modal.js
+++ src/components/ui/Modal.js
@@ -1,14 +1,10 @@
-import Button from '../button/Button';
-import DropDown from '../forms/Dropdown';
-import Title from '../title/Title';
-
-export default function Modal({ onClose, children }) {
+export default function Modal({ onClose, children, className }) {
   return (
     <>
-      <div className="fixed inset-0 z-10 overflow-y-auto transition-all">
+      <div className={`fixed inset-0 z-10 overflow-y-auto transition-all ${className}`}>
         <div className="fixed inset-0 w-full h-full bg-black opacity-40" onClick={onClose}></div>
         <div className="flex items-center min-h-screen px-4 py-8">
-          <div className="relative w-1/2 max-w-lg p-3 mx-auto bg-white border rounded-xl shadow-lg">
+          <div className="relative w-3/4 max-w-lg p-3 mx-auto bg-white border rounded-xl shadow-lg">
             <div className="p-3 mx-auto text-center bg-bgrey rounded-xl">{children}</div>
           </div>
         </div>
