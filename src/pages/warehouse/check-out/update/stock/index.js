b6602b7755c15b859bc976f6efb9be0c4bb5110e
Adrian Rivaldy <adrian.rivaldy@dataon.com>
2022-11-01 21:03:39 +0700

    resize font

--- src/pages/warehouse/check-out/update/stock/index.js
+++ src/pages/warehouse/check-out/update/stock/index.js
@@ -154,15 +154,15 @@ function WarehouseCOStockUpdate() {
         <div className="flex justify-between my-5 gap-3">
           <div className="p-3 rounded-xl border border-cloud w-full">
             <p className="text-xxs mb-3">Wet</p>
-            <p className="text-4xl font-bold">{stockInDetail?.total_wet}</p>
+            <p className="text-2xl font-bold">{stockInDetail?.total_wet}</p>
           </div>
           <div className="p-3 rounded-xl border border-cloud w-full">
             <p className="text-xxs mb-3">DRC</p>
-            <p className="text-4xl font-bold">{stockInDetail?.drc} %</p>
+            <p className="text-2xl font-bold">{stockInDetail?.drc} %</p>
           </div>
           <div className="p-3 rounded-xl border border-cloud w-full">
             <p className="text-xxs mb-3">Dry</p>
-            <p className="text-4xl font-bold">{stockInDetail?.total_dry}</p>
+            <p className="text-2xl font-bold">{stockInDetail?.total_dry}</p>
           </div>
         </div>
         <Divider />
